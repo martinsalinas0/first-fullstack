f269c72e3fb0b9b9173b955af3af5f139840cfea
martinsalinas0 <martinlsalinas@pm.me>
2025-05-18 18:45:11 -0500

    created basic server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-cont 
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+mongoose.connect("mongodb://localhost/products", {});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+const mainRoutes = require("./routes/main.js");
+const productRoutes = require('./routes/products.route.js')
+
+// app.use(mainRoutes);
+app.use(productRoutes)
+app.listen(8000, () => {
+  console.log("Node.js listening on port " + 8000);
+});
